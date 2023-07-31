608e369fb5831a15b627d91bd11e78bbd4366295
Lucas Telles <lltellesss@gmail.com>
2023-07-31 20:47:59 -0300

    countdown context

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
+/* eslint-disable no-unused-vars */
 
 import './App.css'
 import { Title } from './components/Title'
 import { Counter } from './components/Counter'
 
+import useCountdown from './hooks/useCountdonw'
+
 import NewYear from './assets/newyear.jpg'
 
 function App() {
 
+  const [day, hour, minute, second] = useCountdown('Jan 1, 2024 00:00:00')
+
   return (
     <div className='App' style={{backgroundImage:`url(${NewYear})`}}>
       <div className="container">
-        <Title title='Contagem regressiva para 2023' />
+        <Title title='Contagem regressiva para 2024' />
         <div className="countdown-container">
-          <Counter title='Dias' number={2} />
-          <Counter title='Horas' number={2} />
-          <Counter title='Minutos' number={2} />
-          <Counter title='Segundos' number={2} />
+          <Counter title='Dias' number={day} />
+          <Counter title='Horas' number={hour} />
+          <Counter title='Minutos' number={minute} />
+          <Counter title='Segundos' number={second} />
         </div>
       </div>
     </div>
