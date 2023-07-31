ace677c379524d99d7f962e6fb2267a904bf9f1d
Lucas Telles <lltellesss@gmail.com>
2023-07-31 17:55:27 -0300

    basic structure

---
+++ src/components/Counter.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+
+export const Counter = ({title, number}) => {
+  return (
+    <div className="counter">
+        <p className="counter-number">{number}</p>
+        <h3 className="counter-text">{title}</h3>
+    </div>
+  )
+}
---
+++ src/components/Title.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+
+export const Title = ({title}) => {
+  return (
+    <h1>{title}</h1>
+  )
+}
