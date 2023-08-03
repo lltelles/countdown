a7118507c66de4471f5ca2bca13330603ee8aa6a
Lucas Telles <lltellesss@gmail.com>
2023-08-03 17:45:53 -0300

    routes/home form

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 /* eslint-disable no-unused-vars */
 
 import './App.css'
-import { Title } from './components/Title'
-import { Counter } from './components/Counter'
 
-import useCountdown from './hooks/useCountdonw'
+import { Outlet } from 'react-router-dom'
+
 
 import NewYear from './assets/newyear.jpg'
 
 function App() {
 
-  const [day, hour, minute, second] = useCountdown('Jan 1, 2024 00:00:00')
+  
 
   return (
-    <div className='App' style={{backgroundImage:`url(${NewYear})`}}>
+    <div className='App' style={{ backgroundImage: `url(${NewYear})` }}>
       <div className="container">
-        <Title title='Contagem regressiva para 2024' />
-        <div className="countdown-container">
-          <Counter title='Dias' number={day} />
-          <Counter title='Horas' number={hour} />
-          <Counter title='Minutos' number={minute} />
-          <Counter title='Segundos' number={second} />
-        </div>
+    
+        <Outlet />
       </div>
     </div>
   )
---
+++ src/routes/Home.jsx
@@ -0,0 +1,31 @@
+import './Home.css'
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>Monte a sua contagem regressiva!</h2>
+            <form className="countdown-form">
+                <label>
+                    <span>Título:</span>
+                    <input type="text" name="title" placeholder="Dígite o título do evento"/>
+                </label>
+                <label>
+                    <span>Data do evento:</span>
+                    <input type="date" name="date" />
+                </label>
+                <label>
+                    <span>Imagem:</span>
+                    <input type="text" name="image" placeholder="Insita a URL da imagem"/>
+                </label>
+                <label>
+                    <span>Cor do tema:</span>
+                    <input type="color" name="color" />
+                </label>
+                <input type="submit" value="Enviar"/>
+            </form>
+        </div>
+    )
+}
+
+export default Home
